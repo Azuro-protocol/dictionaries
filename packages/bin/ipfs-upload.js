10e3e3b57d5ba374e1c1d5ab7a60e47f83911ca0
Vladimir Ivanov <v.ivanov@azuro.org>
2023-01-05 13:33:50 +0300

    attempt to fix publish

--- packages/bin/ipfs-upload.js
+++ packages/bin/ipfs-upload.js
@@ -4,14 +4,13 @@ const { create, globSource } = require('ipfs-http-client')
 const gateway = 'https://ipfs.bookmaker.xyz/ipfs'
 
 const api = create({ url: "https://ipfs.bookmaker.xyz/api/v0" })
-const fs = require('fs');
+const fs = require('fs')
 
-(async () => {
+;(async () => {
 
   const dicts = []
 
   for await (const file of api.addAll(globSource(path.join('../dictionaries'), '**/*.json'))) {
-    process.stdout.write('.')
     dicts.push(`- ./${file.path} → [${file.cid.toString()}](${gateway}/${file.cid.toString()})`)
   }
 
