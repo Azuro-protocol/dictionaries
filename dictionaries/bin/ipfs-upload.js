ddc5bd3dbb653bc0562af2bd1a8dbaeb75d65a97
Pavel Ivanov <grammka@gmail.com>
2023-01-11 15:15:01 +0100

    fix ipfs-upload.js

--- dictionaries/bin/ipfs-upload.js
+++ dictionaries/bin/ipfs-upload.js
@@ -5,7 +5,7 @@ const { create, globSource } = require('ipfs-http-client')
 
 const IPFS_GATEWAY = 'https://ipfs.bookmaker.xyz/ipfs'
 const API_URL = 'https://ipfs.bookmaker.xyz/api/v0'
-const OUT_DIR = '../dictionaries/out'
+const OUT_DIR = path.resolve(__dirname, '../out/json')
 const FILES_DIRS = [ 'maps', 'arrays' ]
 
 const api = create({ url: API_URL })
@@ -14,22 +14,20 @@ const upload = async () => {
   for (const FILES_DIR of FILES_DIRS) {
     const files = []
 
-    for await (const sourceFile of globSource(path.join(OUT_DIR, FILES_DIR), '**/*.json')) {
-      const ipfsFile = await api.add(sourceFile.content)
+    for await (let outFile of globSource(path.join(OUT_DIR, FILES_DIR), '**/*.json')) {
+      const ipfsFile = await api.add(outFile.content)
 
       const CID = ipfsFile.cid.toString()
-      const filepath = `.${sourceFile.path}`
       const link = `[${CID}](${IPFS_GATEWAY}/${CID})`
 
-      files.push(`- ${filepath} → ${link}`)
-
+      files.push(`- .${outFile.path} → ${link}`)
       process.stdout.write('.')
-
-      const outputPath = path.join(OUT_DIR, FILES_DIR, 'README.md')
-      const content = Buffer.from(files.join('\n\n'))
-
-      await fs.promises.writeFile(outputPath, content)
     }
+
+    const outputPath = path.join(OUT_DIR, FILES_DIR, 'README.md')
+    const content = Buffer.from(files.join('\n\n'))
+
+    await fs.promises.writeFile(outputPath, content)
   }
 }
 
