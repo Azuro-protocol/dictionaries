ff2801ead23a28fe2912ff6486b660f788191465
Pavel Ivanov <grammka@gmail.com>
2023-05-14 15:53:41 +0200

    fix test

--- packages/dictionaries/test.js
+++ packages/dictionaries/test.js
@@ -2,7 +2,7 @@ const { getMarketKey, getMarketName, getMarketDescription, getSelectionName } = 
 
 
 const result = [
-  getMarketKey(1) === '9-1-2',
+  getMarketKey(1) === '9-1-1',
   getMarketName({ outcomeId: 1 }) === 'Both Teams To Score',
   getMarketDescription({ outcomeId: 1 }) === 'A market where you predict whether or not both teams will score at least one goal in the game. You can bet on «yes» if you think that both teams will score or «no» if you think that one of the teams will not score.',
   getSelectionName({ outcomeId: 1 }) === 'Yes',
