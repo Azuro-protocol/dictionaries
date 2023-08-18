497374948346cb4889bf50053b6c111618c6f86f
Pavel Ivanov <grammka@gmail.com>
2023-08-18 13:16:54 +0200

    add error throw if outcome with specified outcomeId not found

--- packages/dictionaries/src/getMarketKey.ts
+++ packages/dictionaries/src/getMarketKey.ts
@@ -3,7 +3,13 @@ import { MARKET_IDS___DONT_GROUP_MARKETS_BY_TEAM_PLAYER_ID } from './constants'
 
 
 const getMarketKey = (outcomeId: string | number): string => {
-  const { marketId, gamePeriodId, gameTypeId, teamPlayerId } = dictionaries.outcomes[outcomeId]
+  const outcome = dictionaries.outcomes[outcomeId]
+
+  if (!outcome) {
+    throw new Error(`Outcome with outcomeId:${outcomeId} not found in dictionaries. Please try update @azuro-org/dictionaries package to the latest version or contact Azuro team for support.`)
+  }
+
+  const { marketId, gamePeriodId, gameTypeId, teamPlayerId } = outcome
 
   // ATTN very important to not change the order of params in the literal!
   //  same logic used for static keys in helpers/conditions/marketRegistry.ts
